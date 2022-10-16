bec99459cc6d92438a5e0c3a4dd251f6ddefc3ab
BotNikos <boss.nikitos1212@mail.ru>
2022-10-16 04:46:54 +0300

    interface library can work with hero struct instead simple variables

--- include/interface.h
+++ include/interface.h
@@ -1,9 +1,7 @@
 #include <iostream>
 void logMessage (std::string message, std::string log[]);
-void reload (char map[][20], int *heroY, int *heroX,
-             int mapSize, int *HP, char *lastCell,
-             std::string log[], int inventory[],
-             int clearInventorySlot, std::string itemList[][2],
+void reload (char map[][20], int mapSize, struct hero *hero,
+             std::string log[], std::string itemList[][2],
 	     int inventoryMode, int inventoryCursorPosition,
              struct enemy enemies[], int enemiesSize,
              int battleMode, struct enemy *battler);
